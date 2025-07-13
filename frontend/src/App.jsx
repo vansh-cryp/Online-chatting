fef1ea60ed942f1c139b871deebcef736c049fb1
vansh-cryp <vanshgusain920@gmail.com>
2025-07-14 00:21:18 +0530

    errors in modules making it incompatible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,10 @@ const App = () => {
       <Navbar/>
 
       <Routes>
-        
-        <Route path="/" element={< HomePage/>} />
-        <Route path="/signup" element={< SignupPage/>} />
-        <Route path="/login" element={< LoginPage/>} />
-        <Route path="/settings" element={< SettingsPage/>} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
         <Route path="/profile" element={<ProfilePage/>} />
 
       </Routes>
